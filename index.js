5b375ab1f207c678e373e582405f1ca6de309805
Gabriel <62681139+gabrielfeliphe@users.noreply.github.com>
2022-09-28 21:03:05 -0300

    working

--- index.js
+++ index.js
@@ -28,47 +28,84 @@ async function main() {
   await page.waitForTimeout(5000);
 
   var absenceCount = 0;
-
-  let today = new Date().toLocaleDateString('pt-br')
-
+ 
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; 
+  var yyyy = today.getFullYear();
+  //today = dd+'/'+mm+'/'+yyyy;
+  today = '2022-08-25'
   console.log(today)
 
   for(var i = 1; i<=6; i++){
-    await page.click(`#frequencia999956 > div > div:nth-child(${i})`);
+    await page.click(`#frequencia999956 > div > div:nth-child(${i}) > a > span`);
+    await page.waitForTimeout(800);
+
+    const data = await page.evaluate(() => {
+      var retorno = '';
+      let x = document.querySelector('#uc9999560 > table > tbody').rows;
+      for(var i = 1; i <= x.length; i++){
+        for(var y=1; y <= x.item(i).cells.length; y++){
+          retorno +=  x.item(i).cells.item(y) + ' | '
+        }
+        retorno += '\n'
+      }
+      return retorno;
+    });
+
+    console.log('data valor '+data)
+
+    // console.log('entrou no for') 
   }
 
-  page.on('response', async (response) => {    
-    if (response.url() == "https://estudante.sesisenai.org.br/api/desempenho/frequencia/999956/7593"){
-        console.log('XHR response received'); 
-        console.log(await response.json()); 
-    }
-  }); 
+  // page.on('response', async (response) => {    
+  //   if (response.url() == "https://estudante.sesisenai.org.br/api/desempenho/frequencia/999956/7593"){
+  //       let x = await response.json(); 
+  //       for (const item of x) {
+  //         //console.log(item.dataAula)
+  //         if(today === item.dataAula && item.presente != 'P'){console.log('Falta | '+item.horarioAula+' | '+item.nomeUc)}
+  //       }
+  //       //console.log(await response.json()); 
+  //   }
+  // }); 
 
-  page.on('response', async (response) => {    
-    if (response.url() == "https://estudante.sesisenai.org.br/api/desempenho/frequencia/999956/7588"){
-        console.log('XHR response received'); 
-        console.log(await response.json()); 
-    }
-  });
+  // page.on('response', async (response) => {    
+  //   if (response.url() == "https://estudante.sesisenai.org.br/api/desempenho/frequencia/999956/7588"){
+  //        let x = await response.json(); 
+  //       for (const item of x) {
+  //         //console.log(item.dataAula)
+  //         if(today === item.dataAula && item.presente != 'P'){console.log('Falta | '+item.horarioAula+' | '+item.nomeUc)}
+  //       }
+  //   }
+  // });
   
-  page.on('response', async (response) => {    
-    if (response.url() == "https://estudante.sesisenai.org.br/api/desempenho/frequencia/999956/7589"){
-        console.log('XHR response received'); 
-        console.log(await response.json()); 
-    }
-  }); 
-  page.on('response', async (response) => {    
-    if (response.url() == "https://estudante.sesisenai.org.br/api/desempenho/frequencia/999956/7591"){
-        console.log('XHR response received'); 
-        console.log(await response.json()); 
-    }
-  }); 
+  // page.on('response', async (response) => {    
+  //   if (response.url() == "https://estudante.sesisenai.org.br/api/desempenho/frequencia/999956/7589"){
+  //        let x = await response.json(); 
+  //       for (const item of x) {
+  //         //console.log(item.dataAula)
+  //         if(today === item.dataAula && item.presente != 'P'){console.log('Falta | '+item.horarioAula+' | '+item.nomeUc)}
+  //       }
+  //   }
+  // }); 
+  // page.on('response', async (response) => {    
+  //   if (response.url() == "https://estudante.sesisenai.org.br/api/desempenho/frequencia/999956/7591"){
+  //        let x = await response.json(); 
+  //       for (const item of x) {
+  //         //console.log(item.dataAula)
+  //         if(today === item.dataAula && item.presente != 'P'){console.log('Falta | '+item.horarioAula+' | '+item.nomeUc)}
+  //       }
+  //   }
+  // }); 
 
-  page.on('response', async (response) => {    
-    if (response.url() == "https://estudante.sesisenai.org.br/api/desempenho/frequencia/999956/7592"){
-        console.log('XHR response received'); 
-        console.log(await response.json()); 
-    }
-  }); 
+  // page.on('response', async (response) => {    
+  //   if (response.url() == "https://estudante.sesisenai.org.br/api/desempenho/frequencia/999956/7592"){
+  //        let x = await response.json(); 
+  //       for (const item of x) {
+  //         //console.log(item.dataAula)
+  //         if(today === item.dataAula && item.presente != 'P'){console.log('Falta | '+item.horarioAula+' | '+item.nomeUc)}
+  //       }
+  //   }
+  // }); 
 }
 main();
