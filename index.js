ef0aee693855eaae0ff563448ff7a97d8c4d07ee
Gabriel Campos <gabriel.campos@fulltechpro.com.br>
2022-09-29 10:33:43 -0300

    update

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.get('/', async function (req, res) {
   res.status(200).json(x)
 })
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server listening`)
- })
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
